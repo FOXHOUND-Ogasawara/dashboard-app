68dbd480d990be46cddd7ebb0a7d12741baa3a39
kazuki.iwai <kazuki.iwai@fox-hound.jp>
2024-12-17 17:43:55 +0900

    一部修正

--- src/data/UserData.ts
+++ src/data/UserData.ts
@@ -1,9 +1,9 @@
 export const userData = [
-    { month: '1月', newUsers: 400, activeUsers: 240 },
-    { month: '2月', newUsers: 300, activeUsers: 139 },
-    { month: '3月', newUsers: 200, activeUsers: 980 },
-    { month: '4月', newUsers: 278, activeUsers: 390 },
-    { month: '5月', newUsers: 189, activeUsers: 480 },
-    { month: '6月', newUsers: 239, activeUsers: 380 },
-    { month: '7月', newUsers: 349, activeUsers: 430 },
-  ];
+  { month: '1月', newUsers: 400, activeUsers: 240 },
+  { month: '2月', newUsers: 300, activeUsers: 139 },
+  { month: '3月', newUsers: 200, activeUsers: 980 },
+  { month: '4月', newUsers: 278, activeUsers: 390 },
+  { month: '5月', newUsers: 189, activeUsers: 480 },
+  { month: '6月', newUsers: 239, activeUsers: 380 },
+  { month: '7月', newUsers: 349, activeUsers: 430 },
+];
