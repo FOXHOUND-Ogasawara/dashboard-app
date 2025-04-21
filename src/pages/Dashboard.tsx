6209d32e1c7f9298673f7d09d7b562e0b9dff5c4
kanato-osajima <kanato.osajima@fox-hound.jp>
2025-04-21 16:07:53 +0900

    追加実装3

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,10 @@
-import { Grid, Paper, Typography } from '@mui/material';
-import React from 'react';
-import UserStatsChart from '../components/UserStatsChart';
-import SalesChart from '../components/SalesChart';
-import RevenueSales from '../components/RevenueSales';
-import CategorySales from '../components/CategorySales';
+import { Grid, Paper, Typography } from "@mui/material";
+import React from "react";
+import UserStatsChart from "../components/UserStatsChart";
+import SalesChart from "../components/SalesChart";
+import RevenueSales from "../components/RevenueSales";
+import CategorySales from "../components/CategorySales";
+import PeriodSales from "../components/PeriodSales";
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,13 +15,10 @@ const Dashboard: React.FC = () => {
       <Grid container spacing={3}>
         <Grid item xs={12} md={6}>
           <Paper sx={{ p: 2 }}>
-            <Typography variant="h6" gutterBottom>
-              
-            </Typography>
+            <Typography variant="h6" gutterBottom></Typography>
             <UserStatsChart />
           </Paper>
         </Grid>
-
 
         <Grid item xs={12} md={6}>
           <Paper sx={{ p: 2 }}>
@@ -37,6 +35,12 @@ const Dashboard: React.FC = () => {
         <Grid item xs={12} md={6}>
           <Paper sx={{ p: 2 }}>
             <CategorySales />
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <Paper sx={{ p: 2 }}>
+            <PeriodSales />
           </Paper>
         </Grid>
       </Grid>
