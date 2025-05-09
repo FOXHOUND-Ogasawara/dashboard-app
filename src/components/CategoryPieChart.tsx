b3e39a4281c2ad08489cb004eb9e9c55d501f575
saito3215 <kazuto.saito@fox-hound.jp>
2025-05-09 14:25:14 +0900

    categoryPieChartの実装

---
+++ src/components/CategoryPieChart.tsx
@@ -0,0 +1,28 @@
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts";
+import { categoryData } from "../data/productData";
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#AA00FF'];
+
+export const CategoryPieChart = () => {
+
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <PieChart>
+        <Pie
+          data={categoryData}
+          dataKey="value"
+          nameKey="category"
+          cx="50%"
+          cy="50%"
+          outerRadius={80}
+          label
+        >
+          {categoryData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
