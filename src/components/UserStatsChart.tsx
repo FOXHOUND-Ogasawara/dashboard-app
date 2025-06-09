cd8d8e03716e8cd28a9fabf6ec9c6088947ceaf7
hayakawakotone <kotone.hayakawa@fox-hound.jp>
2025-06-09 11:37:24 +0900

    ファイル作成、1まで実践

---
+++ src/components/UserStatsChart.tsx
@@ -0,0 +1,17 @@
+ // ①使う機能をここでimport
+
+function UserStatsChart() {
+// ② useStateはコンポーネントの関数の「いちばん上」に書く！
+
+
+// ③ 処理を書く（例：ボタンを押したとき）
+
+
+// ④ returnの中が「画面に見える部分」
+return (
+<div>
+</div>
+);
+}
+
+export default UserStatsChart
