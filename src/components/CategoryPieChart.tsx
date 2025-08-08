9a28202b482e60ef5ff881ebe2dd41717157cdd7
maki-kato-foxhound <maki.kato@fox-hound.jp>
2025-08-08 12:05:00 +0900

    仮の円グラフをCategoryPieChart に記載

--- src/components/CategoryPieChart.tsx
+++ src/components/CategoryPieChart.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React from 'react';
+import { ResponsiveContainer, PieChart, Pie, Cell,Legend } from 'recharts';
+
+const exdata = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 200 },
+  { name: 'Group D', value: 100 },
+  { name: 'Group E', value: 50 },
+];
 
 const CategoryPieChart = () => {
-
+  return (
+    <div style={{ width: '100%', height: 300 }}>
+      <ResponsiveContainer>
+        <PieChart>
+          <Pie data={exdata} dataKey="value" outerRadius={100} label>
+            <Cell fill="red" />
+            <Cell fill="yelow" />
+            <Cell fill="blue" />
+            <Cell fill="green" />
+            <Cell fill="pink" />
+          </Pie>
+          <Legend />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default CategoryPieChart;
