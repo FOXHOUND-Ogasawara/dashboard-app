b408e79b40de01c345a1d5356373187a4b04212a
maki-kato-foxhound <maki.kato@fox-hound.jp>
2025-08-08 11:37:31 +0900

    ④商品ID、商品名、カテゴリー、価格をテーブルで表示。 　・src/pages/のProducts.tsxにproductData.tsをリンクさせる

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,8 +1,35 @@
 import React from "react";
-import { productData } from "../data"; // productData.ts の場所に応じてパス調整
+import { Typography, Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
+import { products } from "../data/productData";
 
 const Products = () => {
-
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        ダッシュボード
+      </Typography>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>商品名</TableCell>
+            <TableCell>カテゴリ</TableCell>
+            <TableCell>価格</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {products.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.id}</TableCell>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{product.category}</TableCell>
+              <TableCell>{product.price} </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 export default Products;
