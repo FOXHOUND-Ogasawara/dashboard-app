7a0b3da93fb4ec5b7d5b5f5509c33eefb81c22f3
RikuAmiya-fox <riku.amiya@fox-hound.jp>
2025-08-22 14:31:36 +0900

    ダッシュボード開発課題タスク②完了

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,8 @@
 import { Paper, Typography } from "@mui/material";
 import Grid from "@mui/material/GridLegacy";
 import React from "react";
-import PlaceholderChart from "../components/PlaceholderChart";
 import UserStatsChart from "../components/UserStatsChart";
+import SalesChart from "../components/SalesChart";
 
 const Dashboard: React.FC = () => {
   return (
@@ -11,7 +11,7 @@ const Dashboard: React.FC = () => {
         ダッシュボード
       </Typography>
       <Grid container spacing={3}>
-        {/* サンプルチャート1 */}
+        {/* ユーザー統計チャート */}
         <Grid item xs={12} md={6}>
           <Paper sx={{ p: 2 }}>
             <Typography variant="h6" gutterBottom>
@@ -20,13 +20,13 @@ const Dashboard: React.FC = () => {
             <UserStatsChart />
           </Paper>
         </Grid>
-        {/* サンプルチャート2 */}
+        {/* 売上データチャート */}
         <Grid item xs={12} md={6}>
           <Paper sx={{ p: 2 }}>
             <Typography variant="h6" gutterBottom>
-              サンプルチャート2
+              売上データ
             </Typography>
-            <PlaceholderChart />
+            <SalesChart />
           </Paper>
         </Grid>
       </Grid>
