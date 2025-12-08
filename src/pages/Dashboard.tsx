901615a4ef79e224a6acce070dc68ba75ffeb358
NishiDaiki <daiki.nishi@fox-hound.jp>
2025-12-08 11:59:12 +0900

    task2実装完了

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,6 +3,8 @@ import Grid from "@mui/material/GridLegacy";
 import React from "react";
 import PlaceholderChart from "../components/PlaceholderChart";
 import UserStatsChart from "../components/UserStatsChart";
+import SalesChart from "../components/SalesChart";
+
 
 const Dashboard: React.FC = () => {
   return (
@@ -24,9 +26,9 @@ const Dashboard: React.FC = () => {
         <Grid item xs={12} md={6}>
           <Paper sx={{ p: 2 }}>
             <Typography variant="h6" gutterBottom>
-              サンプルチャート2
+              売上データ
             </Typography>
-            <PlaceholderChart />
+            <SalesChart />
           </Paper>
         </Grid>
       </Grid>
